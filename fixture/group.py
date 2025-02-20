3fc8a58ae51ce9da07687452dff1e5a15be8fbff
ksenialopatina <topmail888@gmail.com>
2025-02-20 21:17:14 +0700

    Add Fixture and one new test

--- fixture/group.py
+++ fixture/group.py
@@ -24,6 +24,14 @@ class groupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+
+
     def open_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
---
+++ fixture/session.py
@@ -0,0 +1,16 @@
+class sessionHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def logout(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("Logout").click()
+
+    def login(self, username, password):
+        wd = self.app.wd
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
