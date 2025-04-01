3df99380beb5fcdae236218aaf57feb46efe30a5
ksenialopatina <topmail888@gmail.com>
2025-04-01 22:21:46 +0700

    Changed test

--- model/contact.py
+++ model/contact.py
@@ -31,7 +31,30 @@ class Contact:
         self.all_emails_from_home_page = all_emails_from_home_page
 
     def __repr__(self):
-        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
+        return "%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s" % (
+        self.id,
+        self.firstname,
+        self.lastname,
+        self.middlename,
+        self.nickname,
+        self.title,
+        self.company,
+        self.address,
+        self.homenumber,
+        self.mobile,
+        self.work,
+        self.fax,
+        self.email,
+        self.email2,
+        self.bday,
+        self.homepage,
+        self.email3,
+        self.bmonth,
+        self.byear,
+        self.aday,
+        self.amonth,
+        self.ayear
+        )
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname
--- test/test_add_new_number.py
+++ test/test_add_new_number.py
@@ -1,14 +1,47 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
     
-def test_add_new_number(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+testdata = [
+    Contact(
+        firstname=random_string("firstname", 10),
+        middlename=random_string("middlename", 10),
+        lastname=random_string("lastname", 10),
+        nickname=random_string("nickname", 10),
+        title=random_string("title", 10),
+        company=random_string("company", 10),
+        address=random_string("address", 10),
+        homenumber=random_string("homenumber", 10),
+        mobile=random_string("mobile", 10),
+        work=random_string("work", 10),
+        fax=random_string("fax", 10),
+        email=random_string("email", 10),
+        email2=random_string("email2", 10),
+        email3=random_string("email3", 10),
+        bday=str(random.randrange(31) + 1),
+        homepage=random_string("homepage", 10),
+        bmonth=months[random.randrange(len(months) - 1)],
+        byear=random_string("byear", 10),
+        aday=str(random.randrange(31) + 1),
+        amonth=months[random.randrange(len(months) - 1)],
+        ayear=random_string("ayear", 10),
+    )
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_new_number(app, contact):
     old_contacts = app.contact.get_contact_list()
     app.contact.open_form()
-    contact = Contact(firstname="Ksy", middlename="LOP", lastname="lop", nickname="p4oolo", title="title",
-                      company="cft", address="ivanova 8", homenumber="8903678909", mobile="8907543226",
-                      work="test", fax="fax", email="hjbhgc@ghff.hgf", email2="jhhfdd@nhv.jhhf", bday="2",
-                      homepage="home", email3="hgfgfdfd@bvcv.hgg",
-                      bmonth="February", byear="1999", aday="3", amonth="February", ayear="2345")
     app.contact.create_contact(contact)
     app.return_to_home_page()
     assert len(old_contacts) + 1 == app.contact.count()
