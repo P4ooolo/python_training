200abdf3537dbcffb9d2a5314152fdcc18aed855
ksenialopatina <topmail888@gmail.com>
2025-04-05 20:11:58 +0700

    Fix

--- test/test_phones.py
+++ test/test_phones.py
@@ -5,9 +5,9 @@ def test_phones_on_home_page(app):
     contact_from_edit_page = app.contact.get_contact_from_edit_page(0)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.address == contact_from_edit_page.address
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname.strip()
+    assert contact_from_home_page.lastname == contact_from_edit_page.lastname.strip()
+    assert contact_from_home_page.address == contact_from_edit_page.address.strip()
 
 
 # def test_phones_contact_view_page(app):
@@ -20,11 +20,11 @@ def test_phones_on_home_page(app):
 
 
 def clear(s):
-    return re.sub("[() -]", "", s)
+    return re.sub("[(). -]", "", s)
 
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "", map(lambda x: clear(x), filter(lambda x: x is not None, [contact.homenumber, contact.mobile, contact.work]))))
 
 
 def merge_emails_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "", map(lambda x: clear(x), filter(lambda x: x is not None, [contact.email, contact.email2, contact.email3]))))
+    return "\n".join(filter(lambda x: x != "", map(lambda x: x.strip(), filter(lambda x: x is not None, [contact.email, contact.email2, contact.email3]))))
