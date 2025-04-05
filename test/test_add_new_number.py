70ab72615a02d759bc6591f093c7697c4c5775cb
ksenialopatina <topmail888@gmail.com>
2025-04-05 21:38:34 +0700

    Add load test data from files

--- test/test_add_new_number.py
+++ test/test_add_new_number.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 
-def test_add_new_number(app, data_contacts):
+def test_add_new_number(app, json_contacts):
     old_contacts = app.contact.get_contact_list()
     app.contact.open_form()
-    app.contact.create_contact(data_contacts)
+    app.contact.create_contact(json_contacts)
     app.return_to_home_page()
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    old_contacts.append(data_contacts)
+    old_contacts.append(json_contacts)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
